59bc11868f77951099bc57ca28f6ee1267f734b8
alinajunaid <alinajunaidahsan@gmail.com>
2021-10-03 22:09:19 -0400

    test2

---
+++ Test2.py
@@ -0,0 +1,10 @@
+myoperation = input("Please enter operation: ")
+myfirstnumber = input("Please enter first number: ")
+myfirstnumber = input("Please enter first number: ")
+mysecondnumber = input("Please enter second number: ")
+
+if myfirstnumber == "add":
+    print(f"result is: {int(myfirstnumber)+int(mysecondnumber)}")
+else:
+    print("Sorry, I cannot perform that operation")
+

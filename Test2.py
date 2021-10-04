2241e6bfc911cdfea193f6193e282f8a491a4c33
alinajunaid <alinajunaidahsan@gmail.com>
2021-10-03 22:40:03 -0400

    added subtraction

--- Test2.py
+++ Test2.py
@@ -3,8 +3,10 @@ myfirstnumber = input("Please enter first number: ")
 myfirstnumber = input("Please enter first number: ")
 mysecondnumber = input("Please enter second number: ")
 
-if myfirstnumber == "add":
+if myoperation == "add":
     print(f"result is: {int(myfirstnumber)+int(mysecondnumber)}")
+else if myoperation == "subtract":
+    print(f"result is: {int(myfirstnumber)-int(mysecondnumber)}")
 else:
     print("Sorry, I cannot perform that operation")
 
